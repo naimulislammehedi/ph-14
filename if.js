46c3e1823eb493047485621405d9567b2abb753f
Mehedi <mehedi.interface@gmail.com>
2026-04-13 13:00:12 +0600

    14_4 Conditional branching if Else Condition

---
+++ if.js
@@ -0,0 +1,16 @@
+// var biriyaniPrice = 250; 
+
+// if (biriyaniPrice < 300) {
+//     console.log('I will eat biriyani');
+// }
+
+
+
+
+var weight = 40; 
+
+if (weight < 20) {
+    console.log("I will carry it by myself"); 
+} else {
+    console.log("I will ask for help");
+}
