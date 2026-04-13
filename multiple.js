9d828df16baecfac384a8c1cb60b2d2355217ef0
Mehedi <mehedi.interface@gmail.com>
2026-04-13 15:44:01 +0600

    14_5 Multiple condition Logical Operators(&&, ||)

---
+++ multiple.js
@@ -0,0 +1,25 @@
+const salary = 25000; 
+const isBCS = true; 
+
+// if (salary > 20000 && isBCS) {
+//     console.log("I will take this job");
+// } else {
+//     console.log("I will not take this job");
+// }
+
+const dayoff = 2; 
+
+if (salary > 20000 && isBCS && dayoff >= 2) {
+    console.log("I will take this job");
+} else {
+    console.log("I will not take this job");
+}
+
+
+const hasCar = true;
+
+if (salary > 25000 && hasCar == true) {
+    console.log("I will take this job");
+} else {
+    console.log("I will not take this job");
+}
