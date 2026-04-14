09a81369b35bf8b09a3251c0473a6bf0742e50b5
Mehedi <mehedi.interface@gmail.com>
2026-04-14 14:40:59 +0600

    task 1

---
+++ task1.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burgerPrice = 450; 
+
+if (burgerPrice >= 500) {
+    console.log("You get a free Coke!");
+} else {
+    console.log("Coke price: 30tk");
+}
