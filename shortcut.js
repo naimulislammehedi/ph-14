cc0aeb4aa2d02419e406081661983f2367093125
Mehedi <mehedi.interface@gmail.com>
2026-04-14 12:06:17 +0600

    added shortcut

---
+++ shortcut.js
@@ -0,0 +1,15 @@
+const isLeader = true; 
+if (isLeader) {
+    console.log("I am the leader");
+} else {
+    console.log("I am not the leader");
+}
+
+
+const isPassed = false; 
+
+if (isPassed === false) {
+    console.log("You have failed the test");
+} else {
+    console.log("Congratulations! You have passed the test");
+}  
